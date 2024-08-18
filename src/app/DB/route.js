b8866809f6ffc0aee92a0d2a394a152fefa96eb0
RyomenSukuna7 <140910291+RyomenSukuna7@users.noreply.github.com>
2024-08-18 17:56:10 +0530

    Update

--- src/app/DB/route.js
+++ src/app/DB/route.js
@@ -3,49 +3,64 @@ import { writeFile } from 'fs/promises';
 import mongoose from "mongoose";
 
 if (!mongoose.connection.readyState) {
-    mongoose.connect(process.env.DBS);
+    mongoose.connect(process.env.DBS, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => console.log("Connected to database"))
+      .catch((error) => console.error(" connection error:", error));
 }
 
 const schema = new mongoose.Schema({
     ImageName: {
-        type: String
+        type: String,
+        required: true,
     },
 });
 
 export async function POST(req) {
     try {
+        
+
         const product = mongoose.models.image || mongoose.model("image", schema);
         const data = await req.formData();
         const file = data.get("file");
 
         if (!file) {
-            return NextResponse.json({ "error": "Your file not uploaded" });
+            console.log("No file provided");
+            return NextResponse.json({ "error": "Your file was not uploaded" });
         }
 
         const datas = await file.arrayBuffer();
         const convert = Buffer.from(datas);
         const path = `./public/${file.name}`;
 
+        
         const sendDatas = new product({ ImageName: file.name });
         const result = await sendDatas.save();
+       
 
+        
         await writeFile(path, convert);
+    
 
         return NextResponse.json({ success: true });
     } catch (error) {
-        console.error(error);
+        console.error("Error during POST request:", error);
         return NextResponse.json({ success: false, error: error.message });
     }
 }
 
 export async function GET(req) {
     try {
+        console.log("Received GET request");
+
         const product = mongoose.models.image || mongoose.model("image", schema);
         const dataCome = await product.find({});
+        console.log("Data retrieved from database:", dataCome);
 
         return NextResponse.json({ data: dataCome });
     } catch (error) {
-        console.error(error);
+        console.error("Error during GET request:", error);
         return NextResponse.json({ success: false, error: error.message });
     }
 }
