7b573fbfec6f01733e3bc2ba958e41ae15d3c450
RyomenSukuna7 <140910291+RyomenSukuna7@users.noreply.github.com>
2024-08-18 17:29:05 +0530

    Update

--- src/app/DB/route.js
+++ src/app/DB/route.js
@@ -1,40 +1,51 @@
 import { NextResponse } from "next/server";
-import {writeFile} from 'fs/promises';
+import { writeFile } from 'fs/promises';
 import mongoose from "mongoose";
-import { type } from "os";
 
-mongoose.connect(process.env.DBS);
-const schema=new mongoose.Schema({
-        ImageName:{
-            type:String
-        }
-    })
-
-export  async function POST (req){
-    const product=mongoose.models.image || mongoose.model("image",schema);
-    const data=await req.formData();
-    const file=data.get("file");
-    
-    if(!file){
-        return NextResponse.json({"error":"Your file not uploaded"});
-    }
-
-    const datas=await file.arrayBuffer();
-    const convert=Buffer.from(datas);
-    const path=`./public/${file.name}`;
-
-    const sendDatas=new product({ImageName:file.name});
-    const result = await sendDatas.save();
-
-    await writeFile(path,convert);
-
-
-    return NextResponse.json({success:"true"});
+if (!mongoose.connection.readyState) {
+    mongoose.connect(process.env.DBS);
 }
 
-export async function GET(req){
-    const product=mongoose.models.image || mongoose.model("image",schema);
-    const dataCome=await product.find({});
+const schema = new mongoose.Schema({
+    ImageName: {
+        type: String
+    },
+});
 
-    return NextResponse.json({data:dataCome});
+export async function POST(req) {
+    try {
+        const product = mongoose.models.image || mongoose.model("image", schema);
+        const data = await req.formData();
+        const file = data.get("file");
+
+        if (!file) {
+            return NextResponse.json({ "error": "Your file not uploaded" });
+        }
+
+        const datas = await file.arrayBuffer();
+        const convert = Buffer.from(datas);
+        const path = `./public/${file.name}`;
+
+        const sendDatas = new product({ ImageName: file.name });
+        const result = await sendDatas.save();
+
+        await writeFile(path, convert);
+
+        return NextResponse.json({ success: true });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ success: false, error: error.message });
+    }
+}
+
+export async function GET(req) {
+    try {
+        const product = mongoose.models.image || mongoose.model("image", schema);
+        const dataCome = await product.find({});
+
+        return NextResponse.json({ data: dataCome });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ success: false, error: error.message });
+    }
 }
