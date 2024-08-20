8b6a92a264b1843c11277badde28e6c1d28fe245
RyomenSukuna7 <140910291+RyomenSukuna7@users.noreply.github.com>
2024-08-20 13:16:41 +0530

    Add files via upload

---
+++ src/importMessage.js
@@ -0,0 +1,9 @@
+export default function Message(){
+    return(
+        <>
+        
+            <marquee><h3 style={{color:"red"}}>The Price are not fix. They depends on your design</h3></marquee>
+        
+        </>
+    )
+}
