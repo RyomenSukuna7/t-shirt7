343f590b4f910ab846d9723b12ccbc9cbbc39498
RyomenSukuna7 <140910291+RyomenSukuna7@users.noreply.github.com>
2024-08-28 15:02:32 +0530

    Delete

--- src/importMessage.js
+++
@@ -1,9 +0,0 @@
-export default function Message(){
-    return(
-        <>
-        
-            <marquee><h3 style={{color:"red"}}>The prices are not fixed they depend on the design.</h3></marquee>
-        
-        </>
-    )
-}
