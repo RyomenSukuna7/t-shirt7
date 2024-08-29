39d3323351407e16c29ca906c963ff94a688d476
RyomenSukuna7 <140910291+RyomenSukuna7@users.noreply.github.com>
2024-08-29 14:51:51 +0530

    Update

--- src/app/SublimationPhoneCaseDataSend/page.js
+++ src/app/SublimationPhoneCaseDataSend/page.js
@@ -1,7 +1,7 @@
 import CardSublimationPhoneCase from "../SublimationPhoneCase/page";
 
 export default async function SublimationPhoneCaseDataSend() {
-    const response = await fetch("http://localhost:3000/DB");
+    const response = await fetch("https://threadsymphony7.vercel.app/DB");
     const data = await response.json();
 
     return <CardSublimationPhoneCase res={data.data}/>
