884c04581a1c1f1e89ce31b94bb29bf80103c8fb
RyomenSukuna7 <140910291+RyomenSukuna7@users.noreply.github.com>
2024-08-29 14:51:25 +0530

    Update

--- src/app/PhoneGlassCaseDataSend/page.js
+++ src/app/PhoneGlassCaseDataSend/page.js
@@ -1,7 +1,7 @@
 import CardPhoneGlassCase from "../PhoneGlassCase/page";;
 
 export default async function PhoneGlassCaseDataSend() {
-    const response = await fetch("http://localhost:3000/DB");
+    const response = await fetch("https://threadsymphony7.vercel.app/DB");
     const data = await response.json();
 
     return <CardPhoneGlassCase res={data.data}/>
