55f483b6e025f0d3e942010db76956f7a7d13221
RyomenSukuna7 <140910291+RyomenSukuna7@users.noreply.github.com>
2024-08-29 14:52:20 +0530

    Update

--- src/app/dataSend/page.js
+++ src/app/dataSend/page.js
@@ -1,7 +1,7 @@
 import Cards from "../cards";
 
 export default async function DataSend() {
-    const response = await fetch("http://localhost:3000/DB");
+    const response = await fetch("https://threadsymphony7.vercel.app/DB");
     const data = await response.json();
   
     return <Cards res={data.data} />;
