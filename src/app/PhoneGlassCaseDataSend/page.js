ee67ea3817f63575bbb48430d595800fa28d6613
RyomenSukuna7 <140910291+RyomenSukuna7@users.noreply.github.com>
2024-10-09 01:37:38 +0530

    Delete src/app directory

--- src/app/PhoneGlassCaseDataSend/page.js
+++
@@ -1,9 +0,0 @@
-import CardPhoneGlassCase from "../PhoneGlassCase/page";;
-
-export default async function PhoneGlassCaseDataSend() {
-    const response = await fetch("https://threadsymphony7.vercel.app/DB");
-    const data = await response.json();
-
-    return <CardPhoneGlassCase res={data.data}/>
-
-}
--- src/app/dataSend/page.js
+++
@@ -1,9 +0,0 @@
-import Cards from "../cards";
-
-export default async function DataSend() {
-    const response = await fetch("https://threadsymphony7.vercel.app/DB");
-    const data = await response.json();
-  
-    return <Cards res={data.data} />;
-  }
-  
